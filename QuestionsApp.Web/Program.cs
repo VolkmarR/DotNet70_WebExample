b4ad6a708fcae19039125a93c983a511c503e7b3
Volkmar Rigo <volkmar.rigo@infominds.eu>
2023-03-05 19:05:46 +0100

    Register MediatR

--- QuestionsApp.Web/Program.cs
+++ QuestionsApp.Web/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 
 var app = builder.Build();
 

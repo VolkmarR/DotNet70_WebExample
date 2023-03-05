e5b2042f5d572f4f55be6933f94b23c62035200c
Volkmar Rigo <volkmar.rigo@infominds.eu>
2023-03-05 20:09:44 +0100

    Add properties for the database model

--- QuestionsApp.Web/DB/QuestionDB.cs
+++ QuestionsApp.Web/DB/QuestionDB.cs
@@ -1,7 +1,15 @@
-﻿namespace QuestionsApp.Web.DB
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuestionsApp.Web.DB
 {
     public class QuestionDB
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Content { get; set; } = "";
+        public ICollection<VoteDB> Votes { get; set; } = null!;
     }
 
 }
--- QuestionsApp.Web/DB/VoteDB.cs
+++ QuestionsApp.Web/DB/VoteDB.cs
@@ -1,7 +1,15 @@
-﻿namespace QuestionsApp.Web.DB
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuestionsApp.Web.DB
 {
     public class VoteDB
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public int QuestionID { get; set; }
+        public QuestionDB Question { get; set; } = null!;
     }
 
 }

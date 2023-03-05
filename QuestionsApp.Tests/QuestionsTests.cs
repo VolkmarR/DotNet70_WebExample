134169e044ab506abc82e40d35df6e73700ac9f6
Volkmar Rigo <volkmar.rigo@infominds.eu>
2023-03-05 19:43:53 +0100

    Implement tests

--- QuestionsApp.Tests/QuestionsTests.cs
+++ QuestionsApp.Tests/QuestionsTests.cs
@@ -1,9 +1,49 @@
 using FluentAssertions;
+using QuestionsApp.Web.Api.Commands;
+using QuestionsApp.Web.Api.Queries;
 
 namespace QuestionsApp.Tests
 {
     public class QuestionsTests
     {
+        private GetQuestionsQuery GetQuestionsQueryHandler => new();
+        private AskQuestionCommand AskQuestionCommandHandler => new();
+        private VoteForQuestionCommand VoteForQuestionCommandHandler => new();
 
+
+        [Fact]
+        public async void Empty()
+        {
+            var response = await GetQuestionsQueryHandler.Handle(new GetQuestionsRequest(), default);
+            response.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async void OneQuestion()
+        {
+            var askResponse = await AskQuestionCommandHandler.Handle(new AskQuestionRequest { Content = "Dummy Question" }, default);
+            askResponse.Should().NotBeNull();
+
+            var response = await GetQuestionsQueryHandler.Handle(new GetQuestionsRequest(), default);
+            response.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async void OneQuestionAndVote()
+        {
+            var askResponse = await AskQuestionCommandHandler.Handle(new AskQuestionRequest { Content = "Dummy Question" }, default);
+            askResponse.Should().NotBeNull();
+
+            var response = await GetQuestionsQueryHandler.Handle(new GetQuestionsRequest(), default);
+            response.Should().HaveCount(1);
+            response[0].Votes.Should().Be(0);
+
+            var voteResponse = await VoteForQuestionCommandHandler.Handle(new VoteForQuestionRequest { QuestionID = response[0].ID }, default);
+            voteResponse.Should().NotBeNull();
+
+            response = await GetQuestionsQueryHandler.Handle(new GetQuestionsRequest(), default);
+            response.Should().HaveCount(1);
+            response[0].Votes.Should().Be(1);
+        }
     }
 }
